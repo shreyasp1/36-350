3bec555c18c6eb49d02b3d6e86c9215366b70cf6
nicolesp1 <shreyaprakash@Shreyas-MacBook-Pro-2.local>
2018-04-17 13:00:38 -0400

    created model section function that returns the p value of the significant coefficient of a model after running linear regression on that.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,6 +1,15 @@
 generate_data = function(n,p){
   m = matrix(rnorm(n*p), nrow = n, ncol = p)
   resp = rnorm(n)
-  list("matrix" = m, "responses" = resp)
+  return(list("matrix" = m, "responses" = resp))
+}
+
+model_select = function(cov, resp, cutoff){
+  regr.lm = lm(resp ~ cov)
+  coef.p_vals = (summary(regr.lm)$coefficients[,4])[-1]
+  sig.ceof = which(coef.p_vals <= cutoff)
+  regr.coef.lm = lm(resp ~ cov[,sig.ceof])
+  return((summary(regr.coef.lm)$coefficients[,4])[-1])
+  
 }
 
